5bb6d57280f851a5b106f8929e80ffb1439aedbf
Success Chijioke <successceejay@gmail.com>
2026-02-24 23:28:21 +0100

    breedcrum update

--- resources/views/users/carts/carts.blade.php
+++ resources/views/users/carts/carts.blade.php
@@ -239,8 +239,8 @@
             <div class="col-12">
               <nav aria-label="breadcrumb">
                 <ol class="breadcrumb mt-0">
-                  <li class="breadcrumb-item"><a href="#">Home</a></li>
-                  <li class="breadcrumb-item"><a href="#">Shop</a></li>
+                  <li class="breadcrumb-item"><a href="{{ route('users.index') }}">Home</a></li>
+                  <li class="breadcrumb-item"><a href="{{ route('users.products') }}">Shop</a></li>
                   <li class="breadcrumb-item active" aria-current="page">{{ $breadcrumb }}</li>
                 </ol>
               </nav>
