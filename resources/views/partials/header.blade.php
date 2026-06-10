ca6df12617d4455ec8fc5811b72df52e46d80c72
Michael Ozoudeh <jesmikky@gmail.com>
2026-06-10 23:38:48 +0100

    remove announcemmanet

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,7 +1,7 @@
 <header class="ps-header ps-header--2 ps-header--8">
     <div class="ps-noti" style="background: #73c2fb">
         <div class="container">
-            <p class="m-0" style="color:#000">  <marquee> {!! $announcment->content??null !!}</marquee> </p>
+            {{-- <p class="m-0" style="color:#000">  <marquee> {!! $announcment->content??null !!}</marquee> </p> --}}
         </div><a class="ps-noti__close"><i class="icon-cross"></i></a>
     </div>
     <div class="ps-header__top">
